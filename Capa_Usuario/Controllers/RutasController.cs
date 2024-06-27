4cd3570faacb2fa7c7850d42326988f561757ebc
Antony Yerén <desarrollo@cobefar.com.pe>
2024-06-27 14:49:57 -0500

    Se agregó el ALMACÉN DE CUARENTENA en el desplegable ALMACÉN DESTINO.
    Correo: ASIGNAR DATOS EN LA HOJA DE REPARTO
    Signed-off-by: Antony Yerén <desarrollo@cobefar.com.pe>

--- Capa_Usuario/Controllers/RutasController.cs
+++ Capa_Usuario/Controllers/RutasController.cs
@@ -172,7 +172,7 @@ namespace Capa_Usuario.Controllers
                     if (datosOrdenRuta?.TipoRuta == "TA")
                     {
                         nombreVista = "EditarOrdenDeRuta_TA";
-                        filtrosAlm = new string[] { "01", "02", "03", "04", "09", "ALM07", "ALM08" };
+                        filtrosAlm = new string[] { "01", "02", "03", "04", "09", "ALM07", "ALM08","CUAR07" };
                     }
 
                     ViewBag.UsuarioSesion = $"{user.Nombres} {user.Apellidos}";
