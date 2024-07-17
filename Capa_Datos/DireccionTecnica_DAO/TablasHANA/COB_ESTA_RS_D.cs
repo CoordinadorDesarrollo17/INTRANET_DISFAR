3439cb72b456422cbf3a6d52d3cad92f985c8f3d
Antony Yerén <desarrollo@cobefar.com.pe>
2024-07-17 13:20:08 -0500

    Por solicitud de la Dra. Pamela Collahua: Se implementó la consulta a la tabla "Lista de Estado R.S" para que dinámicamente se obtenga los registros para el uso del filtro en el listado de registros sanitarios y a su vez una unión (LEFT JOIN) por cada registro del listado
    Signed-off-by: Antony Yerén <desarrollo@cobefar.com.pe>

---
+++ Capa_Datos/DireccionTecnica_DAO/TablasHANA/COB_ESTA_RS_D.cs
@@ -0,0 +1,47 @@
+﻿using Capa_Entidad.DireccionTecnica_ENT.TablasHANA;
+using Capa_Entidad.DireccionTecnica_ENT.TablasSql;
+using Sap.Data.Hana;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capa_Datos.DireccionTecnica_DAO.TablasHANA
+{
+    public class COB_ESTA_RS_D
+    {
+        readonly Utilitarios uti = new Utilitarios();
+
+        public List<COB_ESTA_RS_E> ListarEstadoRegistrosSanitarios()
+        {
+            DBHelper db = new DBHelper();
+            List<COB_ESTA_RS_E> lista = null;
+
+            string query = @"SELECT TOP 50 ""Code"", ""Name"" FROM " + uti.schemaHana + @"""@COB_ESTA_RS""";
+
+            try
+            {
+                using (HanaDataReader hdr = db.HanaExecuteReaderNoSp(query))
+                {
+                    lista = new List<COB_ESTA_RS_E>();
+
+                    while (hdr.Read())
+                    {
+                        COB_ESTA_RS_E obj = new COB_ESTA_RS_E();
+                        if (!hdr.IsDBNull(0)) { obj.Code = hdr.GetString(0); }
+                        if (!hdr.IsDBNull(1)) { obj.Name = hdr.GetString(1); }
+
+                        lista.Add(obj);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+
+            return lista;
+        }
+    }
+}
