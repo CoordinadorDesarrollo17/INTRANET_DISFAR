1c6dcc7774aaccb3ac17275c39419bc7e68f167c
Soporte Desarrollo (Servidor 40) <soporte.desarrollo@cobefar.com.pe>
2025-03-19 18:01:10 -0500

    feat: actualizando métodos para obtener datos de la tabla Firmas
    Signed-off-by: Soporte Desarrollo (Servidor 40) <soporte.desarrollo@cobefar.com.pe>

---
+++ Capa_Datos/LogHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capa_Datos
+{
+    public static class LogHelper
+    {
+        public static void RegistrarError(Exception ex, string nombreArchivo)
+        {
+            string directorioLogs = new Utilitarios().directorioLogs;
+            File.AppendAllText(directorioLogs + nombreArchivo + ".txt", $"{DateTime.Now}: {ex.Message}\n{ex.StackTrace}\n");
+        }
+    }
+
+}
