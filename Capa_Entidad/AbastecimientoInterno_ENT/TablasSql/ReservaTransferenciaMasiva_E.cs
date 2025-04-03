ae265a0c90bd1339b850c70816142c86f380d2b7
Antony Yerén <desarrollo@cobefar.com.pe>
2025-04-03 11:17:03 -0500

    Apilar transferencias de forma masiva solo cuando aplique filtros a la lista (rack/bloque, posición, nivel)
    Signed-off-by: Antony Yerén <desarrollo@cobefar.com.pe>

---
+++ Capa_Entidad/AbastecimientoInterno_ENT/TablasSql/ReservaTransferenciaMasiva_E.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capa_Entidad.AbastecimientoInterno_ENT.TablasSql
+{
+    public class ReservaTransferenciaMasiva_E
+    {
+        public int DocNumSolicitudTraslado { get; set; }
+        public List<DetalleReservaTransferenciaMasiva_E> Detalles { get; set; }
+    }
+}
