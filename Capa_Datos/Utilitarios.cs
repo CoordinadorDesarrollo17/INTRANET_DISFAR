f53bd80f9f4c55b2e2ba4b25813fb034e210d0e3
Soporte Desarrollo (Servidor 40) <soporte.desarrollo@cobefar.com.pe>
2025-05-31 09:51:32 -0500

    update BD HANA (Utilitarios)

--- Capa_Datos/Utilitarios.cs
+++ Capa_Datos/Utilitarios.cs
@@ -40,8 +40,8 @@ namespace Capa_Datos
 
             // HANA Database Configuration
             this.cadHana = "Server=172.16.55.36:30015;UserName=B1ADMIN;Password=Passw0rd;";
-            //this.schemaHana = "\"BASE_01_02_2025\".";
-            this.schemaHana = "\"B1H_COBEFAR_2018\".";
+            this.schemaHana = "\"BASE_24_05_2025\".";
+            //this.schemaHana = "\"B1H_COBEFAR_2018\".";
             //this.cadHanaOnPremise = Environment.GetEnvironmentVariable("CNX_HANA_ON_PREMISE", EnvironmentVariableTarget.Machine);
             //this.schemaHanaOnPremise = "\"B1H_COBEFAR_2018\".";
             this.BDsql = Environment.GetEnvironmentVariable("BD_PROYECTO_INTRANET", EnvironmentVariableTarget.Machine);
