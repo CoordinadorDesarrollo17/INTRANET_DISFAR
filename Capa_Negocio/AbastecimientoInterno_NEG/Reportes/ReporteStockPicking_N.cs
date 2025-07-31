750e14044c5d325ac9b78e8a53f51f57aee427a5
Antony Yerén <desarrollo@cobefar.com.pe>
2025-07-31 12:44:30 -0500

    fix: update ordenamiento y porcentajes en Reporte Stock Picking

--- Capa_Negocio/AbastecimientoInterno_NEG/Reportes/ReporteStockPicking_N.cs
+++ Capa_Negocio/AbastecimientoInterno_NEG/Reportes/ReporteStockPicking_N.cs
@@ -87,14 +87,17 @@ namespace Capa_Negocio.AbastecimientoInterno_NEG.Reportes
             }).ToList();
 
             return nuevaLista
-                .OrderBy(n => n.StockActualPiezas <= 0)                                                     // Sin stock SAP (NEGRO)
-                .ThenBy(n => n.StockPicking < 0)                                                                   // Stock picking NEGATIVO (no debería pasar frecuentemente) 
-                .ThenBy(n => n.StockActualUnidades == n.StockPicking || n.StockActualUnidades < n.StockMinAbastecimiento)     // Stock SAP al máximo y que no puede cubrir el stock mín. abast., se considera 100% de todo lo que existe en SAP (NEGRO) - NO NECESITA ABASTECIMIENTO
-                .ThenBy(n => n.StockMinAbastecimiento <= 0)                                          // Sin parámetros de stock mín. abast. (GRIS)
-                .ThenBy(n => n.StockPicking > 0 && n.StockPicking >= n.StockMinAbastecimiento)                  // Stock picking cumpliendo el 100% o más del stock mín. abast. (VERDE)
-                .ThenBy(n => n.StockPicking > 0 && n.StockPicking >= n.StockMinAbastecimiento * 0.5M)    // Stock picking por abastecer (>=50% AMARILLO)
-                .ThenBy(n => n.StockPicking > 0 && n.StockPicking < n.StockMinAbastecimiento * 0.5M)      // Stock picking crítico por abastecer (<50% ROJO)
-                .ThenBy(n => n.StockMinAbastecimiento > 0 ? ((decimal)n.StockPicking * 100 / n.StockMinAbastecimiento) : decimal.MaxValue)       // Finalmente, ordena por % de abastecimiento ASC, pero evita división por cero
+                .OrderBy(n => n.StockActualPiezas <= 0)                          // Sin stock SAP (GRIS)
+                .ThenBy(n => n.StockPicking < 0)                                 // Stock picking NEGATIVO (GRIS - no debería pasar frecuentemente) 
+                .ThenBy(n => n.StockMinAbastecimiento <= 0)                      // Sin parámetros de stock mín. abastecimiento (GRIS)
+                .ThenBy(n => n.StockActualUnidades == n.StockPicking)            // No necesita abastecimiento (NEGRO)
+                .ThenBy(n => n.StockActualUnidades < n.StockMinAbastecimiento && n.StockPicking > n.StockMinAbastecimiento * 0.5m)   // Cuando el stock SAP no cubre el stock mín. (NEGRO)
+                .ThenBy(n => n.StockPicking > 0 && n.StockPicking >= n.StockMinAbastecimiento)          // Stock picking cumpliendo el 100% o más del stock mín. abast. (VERDE)
+                .ThenBy(n => n.StockPicking > 0 && n.StockPicking >= n.StockMinAbastecimiento * 0.5M)   // Stock picking por abastecer (>=50% AMARILLO)
+                .ThenBy(n => n.StockPicking > 0 && n.StockPicking < n.StockMinAbastecimiento * 0.5M)    // Stock picking crítico por abastecer (<50% ROJO)
+                .ThenBy(n => n.StockMinAbastecimiento > 0
+                    ? (decimal)n.StockPicking * 100 / n.StockMinAbastecimiento
+                    : decimal.MaxValue)
                 .ToList();
         }
 
