bc77313cbdc00008175fb168ac4f4f5eae1251a6
COBEFAR\TI-2 <soporte01.desarrollo@gmail.com>
2026-03-05 14:32:33 -0500

    Correccion utlitarios

--- Capa_Datos/Utilitarios.cs
+++ Capa_Datos/Utilitarios.cs
@@ -45,7 +45,7 @@ namespace Capa_Datos
             //this.schemaHana = "\"BASE_28_06_2025\".";
 
             this.BDsql = Environment.GetEnvironmentVariable("NOMBRE_BD_PRINCIPAL", EnvironmentVariableTarget.Machine);
-            this.cadSql = Environment.GetEnvironmentVariable("PROYECTO_INTRANET_PRODUCCION", EnvironmentVariableTarget.Machine);
+            this.cadSql = Environment.GetEnvironmentVariable("PROYECTO_INTRANET", EnvironmentVariableTarget.Machine);
             this.cadSql2 = Environment.GetEnvironmentVariable("BD_Operaciones", EnvironmentVariableTarget.Machine);
             CadSql3 = Environment.GetEnvironmentVariable("BD_DT", EnvironmentVariableTarget.Machine);
             CadSql4 = Environment.GetEnvironmentVariable("BD_DOCUMENTOS_REGULATORIOS", EnvironmentVariableTarget.Machine);
