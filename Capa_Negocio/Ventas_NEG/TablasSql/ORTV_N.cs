c8df2d4a16db03b2fd607bde9e70d938d23e831a
COBEFAR\TI-3 <soporte02.desarrollo>
2026-06-23 10:47:36 -0500

    cambiando nombre almacen

--- Capa_Negocio/Ventas_NEG/TablasSql/ORTV_N.cs
+++ Capa_Negocio/Ventas_NEG/TablasSql/ORTV_N.cs
@@ -198,11 +198,11 @@ namespace Capa_Negocio.Ventas_NEG.TablasSql
             //if (string.IsNullOrWhiteSpace(ticket.Agencia)) throw new Exception("Debe llenar la agencia.");
             if (string.IsNullOrWhiteSpace(ticket.DirDestino)) throw new Exception("Debe llenar la dirección de destino.");
             ValidarPersona(ticket);
-            ValidarLugarDeEntrega(ticket, new List<string> { "Almacén 01" });
+            ValidarLugarDeEntrega(ticket, new List<string> { "ALMACEN N°1" });
         }
         private void ValidarArriola(ORTV_E ticket)
         {
-            ValidarLugarDeEntrega(ticket, new List<string> { "Almacén 01" });
+            ValidarLugarDeEntrega(ticket, new List<string> { "ALMACEN N°1" });
         }
         private void ValidarDomicilio(ORTV_E ticket)
         {
@@ -210,11 +210,11 @@ namespace Capa_Negocio.Ventas_NEG.TablasSql
             if (string.IsNullOrWhiteSpace(ticket.DirDestino)) throw new Exception("Debe llenar la dirección de destino.");
             if (ticket.Det3 != null && ticket.Det3.Count >= 2 && !string.IsNullOrWhiteSpace(ticket.Det3[1].Calle) && ticket.Det3[1].Calle.Length > 200) throw new Exception("La dirección de destino excede el límite de 200 caracteres.");
             ValidarPersona(ticket);
-            ValidarLugarDeEntrega(ticket, new List<string> { "Almacén 01" });
+            ValidarLugarDeEntrega(ticket, new List<string> { "ALMACEN N°1" });
         }
         private void ValidarCentro(ORTV_E ticket)
         {
-            ValidarLugarDeEntrega(ticket, new List<string> { "Almacén 01" });
+            ValidarLugarDeEntrega(ticket, new List<string> { "ALMACEN N°1" });
         }
         private void ValidarLugarDeEntrega(ORTV_E ticket, List<string> lugaresValidos)
         {
